e5d0fe98b2c039670924a75cf3bd5cf1304fb2de
Martinoww <martinoww88@gmail.com>
2022-01-06 15:26:57 +0200

    fixed details view path

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import { page } from "./lib.js";
 import { contextDecorator,  updateUserNav } from "./middlewares/render.js";
 import { catalogPage } from "./views/catalog.js";
 import { createPage } from "./views/create.js";
-import { detailsPage } from "./views/details.js.js";
+import { detailsPage } from "./views/details.js";
 import { editPage } from "./views/edit.js";
 import { homePage } from "./views/home.js";
 import { loginPage } from "./views/login.js";
